b0b1c1b429ce04734c131877d76be889387db7e6
Matt Batista <mattbgroovy@gmail.com>
2016-08-16 17:03:08 -0700

    Generate a basic layout using gem

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,5 +1,7 @@
 class VisitorsController < ApplicationController
   def new
     @owner = Owner.new
+    flash.now[:notice] = 'Welcome!'
+    flash.now[:alert] = "#{@owner.name}'s birthday is soon; like in #{@owner.countdown} days!"
   end
 end
