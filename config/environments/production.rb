9403f0129845fe5b9a0981630604d3e35ac67054
Matt Batista <mattbgroovy@gmail.com>
2016-08-19 16:46:19 -0700

    Setup Heroku config variables

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,6 +57,24 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "learn-rails_#{Rails.env}"
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.smtp_settings = {
+    address: "smtp.sendgrid.net",
+    port: 587,
+    domain: Rails.application.secrets.domain_name,
+    authentication: "plain",
+    user_name: Rails.application.secrets.email_provider_username,
+    password: Rails.application.secrets.email_provider_password
+  }
+
+  # ActionMailer Config
+  config.action_mailer.default_url_options = {
+    :host => Rails.application.secrets.domain_name
+  }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
